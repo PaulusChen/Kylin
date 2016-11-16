67c5d09ce6d1bb11b70366047ea7ff3afe1a3376
chenpeng <chenpengsmail@qq.com>
2016-11-16 17:30:49 +0800

    add error code

--- Kylin/KylinTypes.h
+++ Kylin/KylinTypes.h
@@ -31,17 +31,19 @@
 
 typedef enum {
     KE_OK = 0,
-    KE_UNKNOW = -1,
-    KE_NULL_PTR = -2,
-    KE_OUT_OF_RANGE = -3,
-    KE_CONTAINER_FULL = -4,
-    KE_NO_ENTRY = -5,
-    KE_3RD_PART_LIBS_ERROR = -6,
+    KE_SYS_ERR = -1,
+    KE_UNKNOW = -2,
+    KE_NULL_PTR = -3,
+    KE_NO_ENTRY = -4,
+    KE_OUT_OF_RANGE = -5,
+    KE_CONTAINER_FULL = -6,
     KE_DB_ERROR = -7,
     KE_SYNTAX_ERROR = -8,
     KE_SYS_OBJ_CREATE_FAILED = -9,
     KE_SYS_OPT_FAILED = -10,
     KE_BAD_PARAM = -11,
+    KE_3RD_PART_LIBS_ERROR = -12,
+    KE_NO_MEMORY = -13,
     KE_MAX_ERROR_CODE
 } KErrorCode;
 
