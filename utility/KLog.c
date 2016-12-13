4c54a2179152726fcdb759b43a66b947cc0d9c78
chenpeng <chenpengsmail@qq.com>
2016-12-13 10:37:19 +0800

    update format of KLog

--- utility/KLog.c
+++ utility/KLog.c
@@ -19,9 +19,9 @@ void KLog(bool pe,uint32_t type,const char *func,const char *format,...) {
 
     if (pe) {
         int err = errno;
-        syslog(type,"(%s)> %s Errno : [%d] %s",func,errBuf,err,strerror(err));
+        syslog(type,"{%s} %s Errno : [%d] %s",func,errBuf,err,strerror(err));
     } else {
-        syslog(type,"(%s)> %s",func,errBuf);
+        syslog(type,"{%s} %s",func,errBuf);
     }
 
     va_end(arg_ptr);
